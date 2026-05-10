2131aa8f335c3528a40bfd4d5e7c8e336331c3fe
gtbuffalo <gtbuff4lo@gmail.com>
2026-05-10 15:48:11 -0400

    Working writer, with syslog and error messages

---
+++ finder-app/writer.c
@@ -0,0 +1,53 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+#include <syslog.h>
+#include <string.h>
+#include <unistd.h>
+
+void show_usage(char* program_name){
+    printf("\nUsage: %s <writefile> <writestr>\n\n", program_name);
+}
+
+#define OPEN_ERROR -1
+
+int main(int argc, char** argv) {
+    openlog(NULL, 0, LOG_USER);
+
+    if(argc < 3){
+        syslog(LOG_ERR, "Invalid number of arguments %d", argc);
+        closelog();
+
+        fprintf(stderr,"Invalid number of arguments %d", argc);
+
+        show_usage(argv[0]);
+        return 1;
+    }
+
+    
+    char* write_file = argv[1];
+    char* write_string = argv[2];
+    syslog(LOG_INFO,"arguments: write_file=%s, write_string=%s",write_file, write_string);
+ 
+    errno = 0;
+    int target_fd = open(write_file, O_WRONLY | O_CREAT | O_TRUNC);
+    const int error = errno;
+    if(OPEN_ERROR == target_fd) {
+        fprintf(stderr,"ERROR: Cannot open file for writing, %s: %s (%d)\n", write_file, strerror(error), error);
+        syslog(LOG_ERR,"ERROR: Cannot open file for writing, %s: %s (%d)\n", write_file, strerror(error), error);
+    }
+    int bytes_written = 0;
+    if(bytes_written = write(target_fd,write_string, sizeof(write_string))){
+        syslog(LOG_INFO,"Wrote %d bytes to file, %s", bytes_written, write_file);
+    } else {
+        int error = errno;
+        fprintf(stderr,"ERROR: Cannot write to file, %s: %s (%d)\n", write_file, strerror(error), error);
+        syslog(LOG_ERR,"ERROR: Cannot write to file, %s: %s (%d)\n", write_file, strerror(error), error);
+    }
+
+    close(target_fd);
+    closelog();
+    return 0;
+}
